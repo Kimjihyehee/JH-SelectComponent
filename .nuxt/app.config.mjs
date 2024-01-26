10b238a94254a34f5a4ff2934765e4deada92417
김지혜 <kimjihye6245@naver.com>
2024-01-26 16:32:22 +0900

    셀렉트 박스 컴포넌트개발

---
+++ .nuxt/app.config.mjs
@@ -0,0 +1,20 @@
+
+import { updateAppConfig } from '#app/config'
+import { defuFn } from 'defu'
+
+const inlineConfig = {
+  "nuxt": {
+    "buildId": "9ad6eba7-12ff-46b4-b0f4-e7df41914dc7"
+  }
+}
+
+// Vite - webpack is handled directly in #app/config
+if (import.meta.hot) {
+  import.meta.hot.accept((newModule) => {
+    updateAppConfig(newModule.default)
+  })
+}
+
+
+
+export default /*@__PURE__*/ defuFn(inlineConfig)
